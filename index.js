e2de6374437f8906ca057252d00eae269f0cd310
Charles Mainegra <cmainegra@gmail.com>
2018-01-10 19:41:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,10 +67,10 @@ function placeOrder(cardNumber) {
    } 
     
     else {
-      var value = total();
-      console.log(`Your total cost is $${value}, which will be charged to the card ${cardNumber}.`)
+      console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+    }
       cart = [];
       return cart;
-    }
+    
     }
 
