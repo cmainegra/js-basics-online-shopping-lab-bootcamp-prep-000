28fe126783856353aba34fcd9195fc53643d527d
Charles Mainegra <cmainegra@gmail.com>
2018-01-10 19:27:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -55,11 +55,11 @@ function removeFromCart(item) {
       cart.splice(i, 1);
       return cart;
     }
-  }
+  }}
+    else {
   console.log('That item is not in your cart');
-  return cart
 }
-
+}
 function placeOrder(cardNumber) {
   // write your code here
 }
