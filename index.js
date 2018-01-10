7967afd2b4d73c5fe1c6d79381a3b1063741ed8c
Charles Mainegra <cmainegra@gmail.com>
2018-01-10 19:17:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -50,8 +50,13 @@ return total;
 }
 
 function removeFromCart(item) {
-  
-  
+  for (let i = 0; i < cart.length; i++) {
+    if(cart[i].hasOwnProperty(item)) {
+      cart.splice(i, 1);
+      return cart;
+    }
+  }
+  return console.log('That item is not in your cart');
 }
 
 function placeOrder(cardNumber) {
