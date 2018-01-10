66052f9a3a52a9a049507aea73b72e0b71037bf2
Charles Mainegra <cmainegra@gmail.com>
2018-01-10 19:28:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ return total;
 }
 
 function removeFromCart(item) {
+  if(cart.length > 0){
   for (let i = 0; i < cart.length; i++) {
     if(cart[i].hasOwnProperty(item)) {
       cart.splice(i, 1);
@@ -59,7 +60,7 @@ function removeFromCart(item) {
     else {
   console.log('That item is not in your cart');
 }
-
+}
 function placeOrder(cardNumber) {
   // write your code here
 }
